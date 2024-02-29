bec8c79205372334283037b1e9166dc7a3ef1f13
Uchantal <u.chantal@alustudent.com>
2024-02-29 10:49:37 -0800

    re-check the file

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    func = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end="")
-            func += 1
-        except IndexError:
-            break
-    print("")
-    return (func)
+def print_list_integer(my_list=[]):
+    for num in range(len(my_list)):
+        print("{:d}".format(my_list[num]))
