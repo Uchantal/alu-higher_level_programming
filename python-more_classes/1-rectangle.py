25a1818ef02e0c34bc6d2ee6b932c7f3499b2d51
Uchantal <156025831+Uchantal@users.noreply.github.com>
2024-03-11 23:41:59 +0200

    Update 1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python3
-"""Write a class Rectangle that defines a rectangle"""
+"""real definition of a rectagle"""
 
 
 class Rectangle:
-    """Write a class Rectangle that defines a rectangle"""
+    """we are going to initiaize new variables"""
     def __init__(self, width=0, height=0):
+        """initialized width and height"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        return self.__width
+        """retrieve width"""
+        return (self.__width)
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
-        return self.__height
+        """retrieve length"""
+        return (self.__height)
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
-
-
+        self.__height = value
