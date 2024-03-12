f56eb7abf7ec1631d9735874711d633835f33045
Uchantal <156025831+Uchantal@users.noreply.github.com>
2024-03-12 12:00:12 +0200

    reviewed

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""integer validator"""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """created a class"""
+    """Reprsent base geometry."""
 
     def area(self):
-        """new function"""
+        """Not yet implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """another function for validating integer"""
+        """Validate a parameter as an integer.
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
