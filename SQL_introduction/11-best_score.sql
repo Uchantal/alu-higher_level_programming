280475d74373adba57b61aa90c1555d7be7e9d01
Uchantal <156025831+Uchantal@users.noreply.github.com>
2024-03-22 00:19:02 +0200

    select score and name.

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- A script that lists all records.
-SELECT name, score FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
