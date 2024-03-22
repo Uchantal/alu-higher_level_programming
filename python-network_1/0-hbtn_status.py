e7f31f0094c8cf79325b949864d6da7f8f9497f3
Uchantal <156025831+Uchantal@users.noreply.github.com>
2024-03-23 00:49:48 +0200

    Finally

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
-"""This module or script
-fetches https://intranet.hbtn.io/status
-using the urllib library
+# python script that fetches 'https://alu-intranet.hbtn.io/status'
 """
+    fetch 'https://intranet.hbtn.io/status'
+"""
+import urllib.request
 
 
-from urllib import request, error
-
-try:
-    url = 'https://intranet.hbtn.io/status'
-    with request.urlopen(url) as response:
-        response_body = response.read()
-        response_text = response_body.decode('utf-8')
+if __name__ == "__main__":
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
         print("Body response:")
-        print("\t- type:", type(response_body))
-        print("\t- content:", response_body)
-        print("\t- utf8 content:", response_text)
-except error.URLError:
-    custom_status = bytes("Custom status", encoding="utf-8")
-    custom_status_text = custom_status.decode('utf-8')
-    print("Body response:")
-    print("\t- type:", type(custom_status))
-    print("\t- content:", custom_status)
-    print("\t- utf8 content:", custom_status_text)
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
