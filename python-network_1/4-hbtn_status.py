eed6d07541d77f45306f6cd649934ad908a4524d
Uchantal <156025831+Uchantal@users.noreply.github.com>
2024-03-22 18:40:41 +0200

    Update 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,11 +1,12 @@
-
 #!/usr/bin/python3
-"""Python script that fetchs."""
-
-
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
 import requests
-if __name__ == "__main__":
-    requ = requests.get('https://intranet.hbtn.io/status')
+
+if __name__ == '__main__':
+
+    res = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(requ.text)))
-    print("\t- content: {}".format(requ.text))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
