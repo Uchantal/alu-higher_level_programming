c89656f2a66bcf160d98d48dea65b4bbfd1be708
Uchantal <156025831+Uchantal@users.noreply.github.com>
2024-04-07 13:26:43 +0200

    mapping

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
-"""
+"""prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa."""
+
 import sys
-from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for instance in session.query(State).order_by(State.id):
-        for city_ins in instance.cities:
-            print(city_ins.id, city_ins.name, sep=": ", end="")
-            print(" -> " + instance.name)
+    my_engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(my_engine)
+    my_session = Session(my_engine)
+
+    state_name = sys.argv[4]
+
+    state = my_session.query(State).filter(State.name == state_name).first()
+
+    if state is None:
+        print("Not found")
+    else:
+        print(f'{state.id}')
+
+    my_session.close()
