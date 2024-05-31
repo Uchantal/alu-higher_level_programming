4d2700b04f066d47eb353e013c51b559c3e92030
Uchantal <156025831+Uchantal@users.noreply.github.com>
2024-05-31 11:41:00 +0200

    Update 6-completed_tasks.js

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,16 @@
 #!/usr/bin/node
-// A script that computes the number of tasks completed by user id.
-
-const args = process.argv;
-let reqURL = args[2];
-let request = require('request');
-request(reqURL, function (error, response, body) {
-  if (error) {
-    console.log('error:', error);
-  } else {
-    let todos = JSON.parse(body);
-    let dash = {};
-    for (let i = 0; i < todos.length; i++) {
-      let status = (todos[i]['completed']);
-      let key = todos[i]['userId'].toString();
-      if (status) {
-        if (dash[key]) {
-          dash[key]++;
-        } else {
-          dash[key] = 1;
-        }
+const request = require('request');
+request(process.argv[2], (err, res, body) => {
+  if (!err) {
+    const tasks = JSON.parse(body);
+    const completed = {};
+    tasks.forEach(task => {
+      if (task.completed && completed[task.userId] === undefined) {
+        completed[task.userId] = 1;
+      } else if (task.completed) {
+        completed[task.userId] += 1;
       }
-    }
-    console.log(dash);
+    });
+    console.log(completed);
   }
 });
