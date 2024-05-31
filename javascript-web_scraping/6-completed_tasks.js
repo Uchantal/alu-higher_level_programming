b777e31d5ecc5da23d9508e40b9e92e6ccb5c319
Uchantal <156025831+Uchantal@users.noreply.github.com>
2024-05-31 11:33:29 +0200

    Update 6-completed_tasks.js

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,9 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-const fs = require('fs');
-request.get(process.argv[2], (err, response, body) => {
-  if (err) console.log(err);
-  fs.writeFile(process.argv[3], body, (error, res) => {
-    if (error) console.log(error);
-  });
+request(process.argv[2], (err, res, body) => {
+  if (!err) {
+    const tasks = JSON.parse(body);
+    const completed = {};
+    tasks.forEach(task => {
+      if (task.completed && completed[task.userId] === undefined) {
+        completed[task.userId] = 1;
+      } else if (task.completed) {
+        completed[task.userId] += 1;
+      }
+    });
+    console.log(completed);
+  }
 });
